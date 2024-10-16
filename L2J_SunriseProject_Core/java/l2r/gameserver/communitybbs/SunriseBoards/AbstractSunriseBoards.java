53795694978ac93a9873a08d07fbed85c7e51314
Miguel Diaz <42343503+partybrasil@users.noreply.github.com>
2024-10-16 22:06:56 +0200

    first commit

---
+++ L2J_SunriseProject_Core/java/l2r/gameserver/communitybbs/SunriseBoards/AbstractSunriseBoards.java
@@ -0,0 +1,20 @@
+package l2r.gameserver.communitybbs.SunriseBoards;
+
+/**
+ * @author L2GamePlay Team Team
+ * @Website www.L2GamePlay.com
+ */
+public abstract class AbstractSunriseBoards
+{
+	public abstract String getList();
+	
+	public void load()
+	{
+	
+	}
+	
+	public void load(String file)
+	{
+	
+	}
+}
